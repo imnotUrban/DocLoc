e2ec210096c6bdca18c05ebd5124b8824cb69d0f
Diego-Hinrichs <diegooffaa@outlook.com>
2023-10-26 22:59:59 -0300

    demo

---
+++ demo.py
@@ -0,0 +1,30 @@
+import httpx
+import random
+import pandas as pd
+import time
+import json
+import asyncio
+
+# URL de la API
+url = "http://127.0.0.1:8000/geolocalize"
+
+async def enviar_documentos(noticia):
+    async with httpx.AsyncClient(timeout=300) as client:
+        response = await client.post(url, json=noticia) # json=doc, es lo que se envía a la API
+    if response.status_code == 200: # Respuesta buena
+        print(f"Listo: {noticia[0]['title']}")
+    else:
+        print(f"Error al llamar a la API. Código de estado: {response.status_code}")
+        print("Respuesta de la API:", response.text)
+
+with open('noticias.json', 'r') as jf: 
+    noticias = json.load(jf)
+
+async def send():
+    for noticia in noticias:
+        await enviar_documentos([noticia])
+        await asyncio.sleep(15)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send())
