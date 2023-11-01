bbff871ad2da021eaac78fe9c1c9d07c6f00fa66
Diego-Hinrichs <diegooffaa@outlook.com>
2023-10-31 23:25:16 -0300

    Dockerfile front: sin probar

--- BackEnd/config/init.sql
+++ BackEnd/config/init.sql
@@ -6,6 +6,4 @@ GRANT ALL PRIVILEGES ON *.* TO 'admin'@'0.0.0.0';
 FLUSH PRIVILEGES;
 
 -- Crear una base de datos
-CREATE DATABASE IF NOT EXISTS storedb CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE storedb;
-ALTER TABLE documents MODIFY text TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS storedb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
