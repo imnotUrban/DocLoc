9dc3621c03ee862b779c192345361aa8ff893cad
Diego-Hinrichs <diegooffaa@outlook.com>
2023-11-04 14:16:58 -0300

    Count

--- BackEnd/routes/query.py
+++ BackEnd/routes/query.py
@@ -5,19 +5,20 @@ from datetime import datetime
 from models.document import documents
 from schemas.document import Document
 from fastapi import APIRouter, Query
+from pydantic import BaseModel
 
 api = APIRouter()
+
+class QueryOut(BaseModel):
+   doc: List[Document]
+   count: int
 
 @api.get("/all", response_model=List[Document])
 def all():
    return conn.query(documents).all()
 
-@api.get("/count", response_model=List[Document])
-def all():
-   return conn.query(documents).count()
-
-@api.get("/query", response_model=List[Document])
-def filters(from_: str | None = None, to_: str | None = None, cat: str | None = None, page: int = 1):
+@api.get("/query", response_model=QueryOut)
+def filters(all: str = None, from_: str | None = None, to_: str | None = None, cat: str | None = None, page: int | None = 1):
    query = conn.query(documents)
    default_from =  "1970-01-01"
    default_to = datetime.now().strftime('%Y-%m-%d')
@@ -25,28 +26,47 @@ def filters(from_: str | None = None, to_: str | None = None, cat: str | None = 
    start_index = (page - 1) * 10
    end_index = page * 10
 
-   if from_ and to_ and cat: # Si vienen los tres parametros en la query
-      return query.filter(and_(documents.c.date >= from_, documents.c.date <= to_, documents.c.category == cat))[start_index:end_index]
+   if all == "all":
+      return {"doc": query.all(), "count": query.count()}
+
+   elif from_ and to_ and cat: # Si vienen los tres parametros en la query
+      result = query.filter(and_(documents.c.date >= from_, documents.c.date <= to_, documents.c.category == cat))
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
    
    elif from_ and to_:
-      return query.filter(and_(documents.c.date >= from_, documents.c.date <= to_))[start_index:end_index]
+      result = query.filter(and_(documents.c.date >= from_, documents.c.date <= to_))[start_index:end_index]
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
 
    elif from_ and cat:
-      return query.filter(and_(documents.c.date >= from_, documents.c.date <= default_to, documents.c.category == cat))[start_index:end_index]
+      result = query.filter(and_(documents.c.date >= from_, documents.c.date <= default_to, documents.c.category == cat))[start_index:end_index]
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
 
    elif to_ and cat:
-      return query.filter(and_(documents.c.date >= default_from, documents.c.date <= to_, documents.c.category == cat))[start_index:end_index]
+      result = query.filter(and_(documents.c.date >= default_from, documents.c.date <= to_, documents.c.category == cat))[start_index:end_index]
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
 
    elif from_:
-      return query.filter(and_(documents.c.date >= from_, documents.c.date <= default_to))[start_index:end_index]
+      result = query.filter(and_(documents.c.date >= from_, documents.c.date <= default_to))[start_index:end_index]
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
 
    elif to_:
-      return query.filter(and_(documents.c.date >= default_from, documents.c.date <= to_))[start_index:end_index]
+      result = query.filter(and_(documents.c.date >= default_from, documents.c.date <= to_))[start_index:end_index]
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
 
-   elif cat: # Viene solo cat
-      return query.filter(and_(documents.c.date >= default_from, documents.c.date <= default_to, documents.c.category == cat))[start_index:end_index]
+   elif cat: 
+      result = query.filter(and_(documents.c.date >= default_from, documents.c.date <= default_to, documents.c.category == cat))[start_index:end_index]
+      total_count = result.count()
+      return {"doc": result[start_index:end_index], "count":total_count}
 
    else:
-      return query[start_index:end_index]
+      result = query[start_index:end_index]
+      total_count = len(result)
+      return {"doc": result[start_index:end_index], "count":total_count}
 
 
