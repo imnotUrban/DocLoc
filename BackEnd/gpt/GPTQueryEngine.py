89dce088b8062bf3db793ef715d29c4a2defdb3d
Diego-Hinrichs <diegooffaa@outlook.com>
2023-11-14 14:45:47 -0300

    geopy

--- BackEnd/gpt/GPTQueryEngine.py
+++ BackEnd/gpt/GPTQueryEngine.py
@@ -4,7 +4,7 @@ import openai
 from dotenv import load_dotenv
 load_dotenv(dotenv_path="../.env")
 
-openai.api_key = "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"
+openai.api_key = "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"
 class GPTQueryEngine:
     def __init__(self):
         pass
