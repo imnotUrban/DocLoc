515c2ef1d10ce9be441100d3fed1bbffdd7a6813
Marcelo Trivinos <marceloto260@gmail.com>
2023-11-26 18:20:21 -0300

    Add test to navigation buttons

--- FrontEnd/docLocFront/tests-frontend/test_.py
+++ FrontEnd/docLocFront/tests-frontend/test_.py
@@ -47,6 +47,7 @@ def test_all_select(option):
     select_element = driver.find_element(By.CSS_SELECTOR, "#CategorySelect")
     select = Select(select_element)
     select.select_by_value(option)
+
     filter_button = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1p4ja0v > div:nth-child(4) > button").click()
     ActionChains(driver)\
         .click(filter_button)\
@@ -89,3 +90,26 @@ def test_filter_date():
 
     assert to_date <= firts_date and from_date >= firts_date and \
       to_date <= second_date and from_date >= second_date
+
+@pytest.mark.navigation
+def test_navigation_buttons():
+    chrome_options = Options()
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    time.sleep(2)
+
+    title_firts_row = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1o8xpnk > div > table > tbody > tr:nth-child(1) > td.css-hffdsd")
+
+    next_button = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-gmuwbf > div > button:nth-child(3)").click()
+    ActionChains(driver)\
+        .click(next_button)\
+        .perform()
+    
+    previous_button = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-gmuwbf > div > button:nth-child(1)").click()
+    ActionChains(driver)\
+        .click(previous_button)\
+        .perform()
+
+    title_firts_row_2 = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1o8xpnk > div > table > tbody > tr:nth-child(1) > td.css-hffdsd")
+
+    assert title_firts_row.text == title_firts_row_2.text
