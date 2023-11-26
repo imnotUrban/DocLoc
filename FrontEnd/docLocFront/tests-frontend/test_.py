0bb4239634beef28a4702df77f6fca3c90ac43bf
Marcelo Trivinos <marceloto260@gmail.com>
2023-11-26 18:09:39 -0300

    Add test to date filter

--- FrontEnd/docLocFront/tests-frontend/test_.py
+++ FrontEnd/docLocFront/tests-frontend/test_.py
@@ -1,16 +1,18 @@
+from datetime import datetime
+import time
+
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-import time
 
 url = "https://docloc.completoschile.online/"
 
 # Parametrize tests to run with different URLs
 @pytest.mark.parametrize("url", [url])
-#def test_title(browser, url):
 def test_title(url):
     chrome_options = Options()
     driver = webdriver.Chrome(options=chrome_options)
@@ -20,7 +22,7 @@ def test_title(url):
     title = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-14igsv4 > div > p")
     assert title.text == "GEOLOCALIZACIÓN DE DOCUMENTOS TERRITORIALES"
 
-# Prueba de distintas opciones de select para el filtro
+# Different select options for the filter
 @pytest.mark.parametrize("option", [
     "entretenimiento",
     "tecnologia",
@@ -55,3 +57,35 @@ def test_all_select(option):
     second_row = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1o8xpnk > div > table > tbody > tr:nth-child(2) > td:nth-child(2)")
     assert first_row.text == option.upper() and second_row.text == option.upper()
 
+@pytest.mark.date
+def test_filter_date():
+    chrome_options = Options()
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    time.sleep(2)
+
+    date_to = "01-01-2019"
+    date_from = "31-12-2022"
+
+    input_to = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1p4ja0v > div:nth-child(2) > input[type=date]")
+    input_to.send_keys(date_to)
+
+    input_from = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1p4ja0v > div:nth-child(3) > input[type=date]")
+    input_from.send_keys(date_from)
+
+    filter_button = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1p4ja0v > div:nth-child(4) > button").click()
+    ActionChains(driver)\
+        .click(filter_button)\
+        .perform()
+    time.sleep(2)
+    
+    first_row = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1o8xpnk > div > table > tbody > tr:nth-child(1) > td:nth-child(3)")
+    second_row = driver.find_element(By.CSS_SELECTOR, "#root > div > div.css-k008qs > div.css-n8bf2u > div > div.css-1o8xpnk > div > table > tbody > tr:nth-child(2) > td:nth-child(3)")
+    
+    to_date = datetime.strptime(date_to, "%d-%m-%Y")
+    from_date = datetime.strptime(date_from, "%d-%m-%Y")
+    firts_date = datetime.strptime(first_row.text, "%Y-%m-%d")
+    second_date = datetime.strptime(second_row.text, "%Y-%m-%d")
+
+    assert to_date <= firts_date and from_date >= firts_date and \
+      to_date <= second_date and from_date >= second_date
